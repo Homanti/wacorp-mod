5f5e444436ae44eae994ebb41718d3aba1dd4a3f
Homanti <homanti0mb2@gmail.com>
2026-01-04 16:56:16 -0800

    хуйня

--- src/main/java/org/homanti/wacorp/Wacorp.java
+++ src/main/java/org/homanti/wacorp/Wacorp.java
@@ -2,13 +2,14 @@ package org.homanti.wacorp;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-//import org.homanti.wacorp.render.SmartphoneScreen;
-//import org.homanti.wacorp.client.registry.ModCommands;
+import org.homanti.wacorp.render.SmartphoneScreen;
+import org.homanti.wacorp.client.registry.ModCommands;
 import org.homanti.wacorp.client.registry.ModCreativeTabs;
 import org.homanti.wacorp.client.registry.ModItems;
 
@@ -23,14 +24,22 @@ public class Wacorp {
         ModCreativeTabs.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
-//        MinecraftForge.EVENT_BUS.register(new ModCommands());
+        MinecraftForge.EVENT_BUS.register(new ModCommands());
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void registerKeys(RegisterKeyMappingsEvent event) {
+            event.register(ModKeybindings.SMARTPHONE_HOME);
+        }
     }
 
     @Mod.EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
     public static class ClientForgeEvents {
         @SubscribeEvent
         public static void onClientLogout(ClientPlayerNetworkEvent.LoggingOut event) {
-//            SmartphoneScreen.handleWorldUnload();
+            SmartphoneScreen.handleWorldUnload();
         }
     }
 }
--- src/main/java/org/homanti/wacorp/item/devices/SmartphoneItem.java
+++ src/main/java/org/homanti/wacorp/item/devices/SmartphoneItem.java
@@ -1,20 +1,28 @@
 package org.homanti.wacorp.item.devices;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.homanti.wacorp.render.SmartphoneScreen;
 
 public class SmartphoneItem extends Item {
     public SmartphoneItem(Properties properties) {
         super(properties);
     }
 
-//    @Override
-//    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-//        if (level.isClientSide) {
-//            Minecraft minecraft = Minecraft.getInstance();
-//
-//            minecraft.setScreen(new SmartphoneScreen(Component.translatable("screen.wacorp.smartphone")));
-//        }
-//
-//        return InteractionResultHolder.success(player.getItemInHand(hand));
-//    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if (level.isClientSide) {
+            Minecraft minecraft = Minecraft.getInstance();
+
+            minecraft.setScreen(new SmartphoneScreen(Component.translatable("screen.wacorp.smartphone")));
+        }
+
+        return InteractionResultHolder.success(player.getItemInHand(hand));
+    }
 }
